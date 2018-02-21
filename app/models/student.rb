a2191a0ac3ac64bdc8a95533a54db4d9515931f2
Brittany Morris <brittmmendez@gmail.com>
2018-02-21 19:42:45 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -14,11 +14,11 @@ class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
 
-  def self.search(student)
-    if student=''
+  def self.search(search)
+    if search=''
       @student=Student.all
     else
-      @student=Student.find_by_name("rob").map {|s| s.name}
+      @student= where(['lower(name) LIKE ?', "%#{search}%"])
     end
 
   end
